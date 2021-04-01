9ce375c23e4f2f3884d018c2baca9f5efc570e82
Samantha Sheets <70865545+7424243@users.noreply.github.com>
2021-04-01 15:35:36 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const style = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+style.color = "blue";
+
+ReactDOM.render(
+  <h1 style={style}>Hello World!</h1>,
+  document.getElementById("root")
+);
